7f09ea90181bd288dbaea78e761e8bb7f7da23aa
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-05-13 14:50:26 +0330

    initial layout2

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,9 @@
+export default function ProtectedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main>
+    <div>back</div>
+    {children}</main>;
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 const protectedR = ["/passangers"];
 export default function middleware(req: NextRequest) {
-  if (protectedR.includes(req.nextUrl.pathname)) {
-    const absoluteUrl = new URL("/", req.nextUrl.origin);
-    return NextResponse.redirect(absoluteUrl.toString());
-  }
+  // if (protectedR.includes(req.nextUrl.pathname)) {
+  //   const absoluteUrl = new URL("/", req.nextUrl.origin);
+  //   return NextResponse.redirect(absoluteUrl.toString());
+  // }
 }
